f3e8df6d28c69ae722cc274cfed807cbd8daabc4
lionking123456789 <juniorforova0@gmail.com>
2020-06-19 13:07:54 +1000

    grass-roots subdomain

---
+++ lio sub/lionub-subdomain-scanner-master/README.md
@@ -0,0 +1,18 @@
+# CSub Subdomain Scanner
+
+*#python* *#subdomain* *#scanner* *#finder* *#tool* *#information* *#gathering* *#tutorial* *#python3* *#scanning* *#how* *#to*
+*#scan* *#subs* *#sub* *#domains* *#info-sec* *#security* *#hacking* *#hacks* *#hack*
+
+## Code Walk-through & Learning resources links
+**YouTube tutorial: https://bit.ly/2DGzBCm**
+
+**For python tutorials: https://bit.ly/2U58Lt9**
+
+**Python_AND_Hacking subreddit: https://bit.ly/2Uf3gbw**
+
+## Become a Patreon - support the channel
+*Become a Patreon and support this channel, so that it can keep creating great content in the future* **LINK: https://www.patreon.com/w3w3w3**
+
+## Built With
+
+* **Python 3.6.5** - [https://www.python.org/](https://www.python.org/)
---
+++ lio sub/lionub_subdomain_scanner.py
@@ -0,0 +1,81 @@
+#// Don't forget to hit SUBSCRIBE, LIKE, COMMENT, SHARE! and LEARN! its good to learn! :)
+
+
+'''imports'''
+import requests
+import time
+import urllib3
+import sys
+
+'''software version'''
+version = 2.2
+
+'''take in target/output values'''
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--domain', type=str, required=True, help='Target Domain.')
+    parser.add_argument('-o', '--output', type=str, required=False, help='Output to file.')
+    return parser.parse_args()
+
+
+'''banner for start of program'''
+def banner():
+    global version
+    print('Name: Lionub')
+    print(f'Version: {version}')
+    print('Copyright: (png internethub)')
+    print('coded by ')
+    print('MIT Licence: Licence in repository')
+    print('Description: PNG internet hub USE THIS FOR EDUCATIONAL USE WE NOT
+    	print('responsible for any of your wrong full action. :)')
+    time.sleep(1)
+
+
+#parse host from scheme, to use for certificate transparency abuse
+def parse_url(url):
+    try:
+        host = urllib3.util.url.parse_url(url).host
+    except Exception as e:
+        print('[*] Invalid domain, try again..')
+        sys.exit(1)
+    return host
+
+def write_subs_to_file(subdomain, output_file):
+    with open(output_file, 'a') as fp:
+        fp.write(subdomain + '\n')
+        fp.close()
+
+
+def main():
+    banner()
+    subdomains = []
+
+    args = parse_args()
+    target = parse_url(args.domain)
+    output = args.output
+
+    req = requests.get(f'https://crt.sh/?q=%.{target}&output=json')
+
+    if req.status_code != 200:
+        print('[*] Information not available!')
+        sys.exit(1)
+
+    for (key,value) in enumerate(req.json()):
+        subdomains.append(value['name_value'])
+
+    print(f"\n[!] ****** TARGET: {target} ****** [!] \n")
+
+
+    subs = sorted(set(subdomains))
+
+    for s in subs:
+        print(f'[*] {s}\n')
+        if output is not None:
+            write_subs_to_file(s, output)
+
+    print("\n\n[**] Lionub is complete, all subdomains have been found.")
+
+
+if __name__=='__main__':
+    main()
